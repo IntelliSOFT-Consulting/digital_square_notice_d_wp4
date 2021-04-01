4432767e7f1569b1b7a7ab72ab8f716bfd6eaafa
Amolo <railamolo@gmail.com>
2021-04-01 10:11:34 +0300

    Added the PLIR widget to a sample OpenMRS MFE

---
+++ openmrs-esm-txpvls/src/TxPVLS/components/ResultsCard.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Tile }  from 'carbon-components-react' 
+
+
+export default function ResultsCard({results, reportingPeriod}) {
+
+
+    return (
+        <>
+            <Tile>
+                <h4><u>{results.measure}</u></h4>
+                <br/>
+                
+                <p>From: <b>{reportingPeriod.start}</b>;  To: <b>{reportingPeriod.end}</b></p>
+                <br/>
+
+                {results.group[0].population.map((population) => {
+                   return <p key={String(population.code.coding[0].code)}>{population.code.coding[0].display}: {population.count}</p>
+                })}
+            
+                <br/>
+                <hr/>
+                <h5>
+					Measure Score: {results.group[0].measureScore ? (results.group[0].measureScore.value).toFixed(2):  "0" }
+				</h5>
+            </Tile>
+        </>
+    )
+}
---
+++ openmrs-esm-txpvls/src/link.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ConfigurableLink } from '@openmrs/esm-react-utils';
+
+export default () => (
+  <ConfigurableLink to="${openmrsSpaBase}/txpvls" className="bx--side-nav__link">
+    TxPVLS Report
+  </ConfigurableLink>
+);

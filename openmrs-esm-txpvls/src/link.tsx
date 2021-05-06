41fd045ffdac6aced8818b8c69920e7de956343d
Brian Amolo <bamolo@li1030-72.members.linode.com>
2021-05-06 08:08:05 +0000

    UI: fix sidebar links

--- openmrs-esm-txpvls/src/link.tsx
+++ openmrs-esm-txpvls/src/link.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { ConfigurableLink } from '@openmrs/esm-react-utils';
 
 export default () => (
-  <ConfigurableLink to="${openmrsSpaBase}/txpvls" className="bx--side-nav__link">
+  <ConfigurableLink to="${openmrsSpaBase}/home" className="bx--side-nav__link">
     TxPVLS Report
   </ConfigurableLink>
 );
